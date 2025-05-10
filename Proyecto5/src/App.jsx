d9804c0c17cf2b94df2d0f950695d5b16841f36c
alvarogatica <alvaroandresgaticariquelme@gmail.com>
2025-05-09 23:43:56 -0400

    errorboundary test en archivo App.jsx

--- Proyecto5/src/App.jsx
+++ Proyecto5/src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 import RandomMeal from "./components/RandomMeal";
+
+// function BrokenComponent() {
+//   throw new Error("This is a broken component");
+// }
 
 function App() {
   return (
+    <ErrorBoundary>
     <div className="App">
       <RandomMeal />
+      {/* <BrokenComponent /> */}
     </div>
+    </ErrorBoundary>
   );
 }
 
